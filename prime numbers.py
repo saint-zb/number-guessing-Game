bd7e17bf6cdce3c0da9a0b8381a2bff2f1d279ee
saint-zb <aabdulhazeeb@gmail.com>
2024-09-13 04:29:56 -0700

    Update prime numbers.py

--- prime numbers.py
+++ prime numbers.py
@@ -9,10 +9,4 @@ is_prime(10)
 """the range function starts at 0 by default
  and stops at the "lastnumber-1""ie it is not inclusie 
  of the stop arg"""
-#     if num%2 != 0 and num % 1==0:
-#         print(True)
-#     else:
-#         print(False)
-#
-# for _ in range(100):
-#     is_prime(_)
+# 
